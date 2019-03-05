cb9229de101d68794baf72cad52eb393b9463f98
edgarated <[chukwuebukaaneke22@gmail.com]>
2019-03-05 18:21:51 +0100

    practiced getElementById

---
+++ app.js
@@ -0,0 +1,7 @@
+let doc = document.getElementById("page-banner");
+// console.log(doc);
+// <div id="page-banner">…</div>
+
+let list =document.getElementById("book-list");
+console.log(list);
+//<div id="book-list">…</div>
