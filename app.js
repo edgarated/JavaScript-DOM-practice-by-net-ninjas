4626428275c26b4500e1be74775c48d2b751fcf6
edgarated <[chukwuebukaaneke22@gmail.com]>
2019-03-06 20:27:49 +0100

    DOM Events

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-hello
+let h2 = document.querySelector("#book-list h2");
+//when h2 tag is clicked it displays the properties
+h2.addEventListener("click", function(e){
+console.log(e.target);
+console.log(e);
+
+
+})
+
+
+
+//This event listener deletes  an item when the delete button is clicked
+const btns = document.querySelectorAll("#book-list .delete");
+btns.forEach(function(btn){
+btn.addEventListener("click" , function(e){
+
+const li = e.target.parentElement;
+  seebtns = li.parentElement.removeChild(li)
+  console.log(seebtns);
+
+});
+
+});
