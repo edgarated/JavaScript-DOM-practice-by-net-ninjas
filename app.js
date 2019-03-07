187f8b3d0c144612fcdd1846b5d1545dfd84ecc2
edgarated <[chukwuebukaaneke22@gmail.com]>
2019-03-07 15:09:57 +0100

    creating DOM elements

--- app.js
+++ app.js
@@ -1,28 +1,36 @@
 //Delete a button
-const list = document.querySelectorAll("#book-list ul");
-list.forEach(function(lists){
+const list = document.querySelector("#book-list ul");
+list.addEventListener("click" , function(e){
 
-  lists.addEventListener("click", function(e){
+  if(e.target.className == "delete");
+  const li = e.target.parentElement;
+  li.parentElement.removeChild(li);
 
-   if(e.target.className == "delete"){
-   const li = e.target.parentElement;
-   li.parentElement.removeChild(li);
-
-   }
-
-  })
-
-})
-
+});
 
 //Add book list
 const addForm = document.forms["add-book"];
 addForm.addEventListener("submit", function(e){
- 
-//    //prevents the default action
+ //prevents the default action
   e.preventDefault();
 const value = addForm.querySelector('input[type="text"]').value;
-console.log(value);
-})
+
+//create elements
+const li = document.createElement("li");
+const bookName = document.createElement("span");
+const deleteBtn = document.createElement("span");
+
+//add content
+deleteBtn.textContent="delete";
+bookName.textContent = value
+
+// // append to DOM 
+li.appendChild(bookName);
+li.appendChild(deleteBtn);
+list.appendChild(li);
+
+});
+
+
 
 
